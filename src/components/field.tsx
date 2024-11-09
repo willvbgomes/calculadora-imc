e32c8d39db94d11d0bc6aa7297583a03c5ca10f1
William Vilas Boas Gomes <willzgomes@gmail.com>
2024-11-08 21:31:27 -0300

    update field component

--- src/components/field.tsx
+++ src/components/field.tsx
@@ -7,6 +7,7 @@ import {
   FormMessage,
 } from './ui/form'
 import { Input } from './ui/input'
+import { useBMIContext } from '@/hooks/bmi-context'
 
 type FieldProps = {
   label: string
@@ -18,23 +19,28 @@ type FieldProps = {
   }>
 }
 
-export const Field = ({ control, label, name, placeholder }: FieldProps) => (
-  <FormField
-    control={control}
-    name={name}
-    render={({ field }) => (
-      <FormItem className="relative">
-        <FormLabel className="mt-1.5 w-1/3">{label}:</FormLabel>
-        <FormControl>
-          <Input
-            type="text"
-            className="text-center"
-            placeholder={placeholder}
-            {...field}
-          />
-        </FormControl>
-        <FormMessage className="absolute top-[95%] text-xs" />
-      </FormItem>
-    )}
-  />
-)
+export const Field = ({ control, label, name, placeholder }: FieldProps) => {
+  const { data } = useBMIContext()
+
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="relative">
+          <FormLabel className="mt-1.5 w-1/3">{label}:</FormLabel>
+          <FormControl>
+            <Input
+              type="text"
+              className="text-center"
+              placeholder={placeholder}
+              disabled={data ? true : false}
+              {...field}
+            />
+          </FormControl>
+          <FormMessage className="absolute top-[95%] text-xs" />
+        </FormItem>
+      )}
+    />
+  )
+}
