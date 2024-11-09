9dbf4fae48d7e7eeb4f52f78c92597915a9c37e4
William Vilas Boas Gomes <willzgomes@gmail.com>
2024-11-08 21:33:47 -0300

    update app: add bmi form and result sections

--- src/app.tsx
+++ src/app.tsx
@@ -1,1 +1,19 @@
-export const App = () => <></>
+import { BMIForm } from './components/bmi-form'
+import { BMIProvider } from './contexts/bmi-provider'
+import { Result } from './components/result'
+
+export const App = () => {
+  return (
+    <main className="m-auto flex min-h-dvh max-w-xs flex-col items-center justify-center gap-8 sm:min-h-screen sm:max-w-sm">
+      <BMIProvider>
+        <section className="w-full">
+          <BMIForm />
+        </section>
+
+        <section className="flex h-28 w-full items-center">
+          <Result />
+        </section>
+      </BMIProvider>
+    </main>
+  )
+}
