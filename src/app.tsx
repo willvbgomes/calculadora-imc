a08f0f4fa1543b8f3eead9495e3e6c51947502a1
William Vilas Boas Gomes <willzgomes@gmail.com>
2024-11-11 23:15:39 -0300

    feat: add dark/light mode

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,25 @@
 import { BMIForm } from './components/bmi-form'
 import { BMIProvider } from './contexts/bmi-provider'
 import { Result } from './components/result'
+import { ThemeProvider } from './contexts/theme-provider'
+import { ThemeToggle } from './components/theme-toggle'
 
 export const App = () => {
   return (
-    <main className="m-auto flex min-h-dvh max-w-xs flex-col items-center justify-center gap-8 sm:min-h-screen sm:max-w-sm">
-      <BMIProvider>
-        <section className="w-full">
-          <BMIForm />
-        </section>
+    <ThemeProvider defaultTheme="dark" storageKey="bmi-theme">
+      <main className="m-auto flex min-h-dvh max-w-xs flex-col items-center justify-center gap-8 sm:min-h-screen sm:max-w-sm">
+        <ThemeToggle />
 
-        <section className="flex h-28 w-full items-center">
-          <Result />
-        </section>
-      </BMIProvider>
-    </main>
+        <BMIProvider>
+          <section className="w-full">
+            <BMIForm />
+          </section>
+
+          <section className="flex h-28 w-full items-center">
+            <Result />
+          </section>
+        </BMIProvider>
+      </main>
+    </ThemeProvider>
   )
 }
